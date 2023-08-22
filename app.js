7c719268c770496df32c0f0632fead67049841f1
Christopher <csbroox@gmail.com>
2023-08-22 10:37:42 -0500

    pretty lost

--- app.js
+++ app.js
@@ -33,23 +33,60 @@ const freelancers = [
 //create variable to calculate average as new entries populate list
 
 //add freelancers to list one at a time
-let count = 0;
-const list = [];
-const n = setInterval(function(){
-  const newName = freelancers[count];
-  list.push(newName);
-  count++;
-  console.log(list);
-  if (list.length === freelancers.length){
-    clearInterval(n);
-  }
+// let count = 0;
+// const list = [];
+// const n = setInterval(function(){
+//   const newName = freelancers[count];
+//   list.push(newName);
+//   count++;
+//   console.log(list);
+//   if (list.length === freelancers.length){
+//     clearInterval(n);
+//   }
   
-}, 3000);
-function render(){
+// }, 3000);
+// function render(){
   
-  box.innerHTML = n;
-  return n;
-}
-render();
+//   box.innerHTML = n;
+//   return n;
+// }
+// render();
 
 //create running average as prices populate list
+const colors = ['red', 'blue', 'green', 'yellow'];
+const sizes = ['sm', 'md', 'lg', 'xl'];
+const types = ['square', 'circle'];
+
+function randomShape(){
+  const colorIdx = Math.floor(Math.random()*colors.length);
+  const sizeIdx = Math.floor(Math.random()*sizes.length);
+  const typeIdx = Math.floor(Math.random()*types.length);
+  const shape = {
+    color: colors[colorIdx],
+    size: sizes[sizeIdx],
+    type: types[typeIdx]
+  };
+  return shape;
+}
+
+const shapes = [
+  randomShape(),
+  randomShape()
+];
+
+function render(){
+  const html = shapes.map(function(shape, idx){
+    return `<div class='${shape.color} ${shape.size} ${shape.type}'>${idx}</div>`;
+  });
+  box.innerHTML = html.join('');
+}
+
+render();
+const i = setInterval(function(){
+  const newShape = randomShape();
+  shapes.push(newShape);
+  render();
+  if (shapes.length > 20){
+    clearInterval(i);
+  }
+}, 2000);
